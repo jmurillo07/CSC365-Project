8506026395823ed1d9345a64abd5f4dfeeed68a8
jmurillo07 <64672775+jmurillo07@users.noreply.github.com>
2023-06-06 00:20:07 -0700

    Create entryGenerator.py

---
+++ entryGenerator.py
@@ -0,0 +1,27 @@
+from faker import Faker
+import random
+import sqlalchemy
+from src import database as db
+
+fake = Faker()
+num_entries = 1000000000
+engine = sqlalchemy.create_engine(db.database_connection_url())
+
+with engine.begin() as conn:
+    for _ in range(num_entries):
+        first_name = fake.first_name()
+        last_name = fake.last_name()
+        height = random.randint(1, 9999)
+        reach = random.randint(1, 9999)
+        stance_id = random.choice([1, 2, 3])
+
+        # allowing for duplicates, inserting all faker entries into the fighters table
+        conn.execute(
+            sqlalchemy.insert(db.fighters)
+            .values(first_name=first_name,
+                    last_name=last_name,
+                    height=height,
+                    reach=reach,
+                    stance_id=stance_id))
+
+
